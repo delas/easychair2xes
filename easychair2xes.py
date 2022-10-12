98afd9102f2b297def31e417c46c6926ac5019f9
Andrea Burattin <delas@users.noreply.github.com>
2022-10-12 11:18:22 +0200

    Update easychair2xes.py

--- easychair2xes.py
+++ easychair2xes.py
@@ -6,16 +6,16 @@ import random
 
 # The log as reported in the "Events" menu on EasyChair
 log = """
-2023-01-01  10:00 review by Person1 on paper 1 Person1
-2023-01-02  10:05 comment on paper 2 Person2
-2023-01-03  10:10 review by Person3 on paper 3  Person3
-2023-01-04  10:15 comment on paper 4 Person4
-2023-01-05  10:20 review by Person5 on paper 5 Person5
-2023-01-06  10:25 review by Person6 on paper 6 Person6
-2023-01-07  10:30 review by Person7 on paper 7  Person7
-2023-01-08  10:35 Decision REJECT on paper 8 Person8
-2023-01-09  10:40 Decision REJECT on paper 9 Person9
-2023-01-10  10:45 Decision ACCEPT on paper 10 Perso10
+2023-01-01	10:00	review by Person1 on paper 1	Person1
+2023-01-02	10:05	comment on paper 2	Person2
+2023-01-03	10:10	review by Person3 on paper 3	Person3
+2023-01-04	10:15	comment on paper 4	Person4
+2023-01-05	10:20	review by Person5 on paper 5	Person5
+2023-01-06	10:25 	review by Person6 on paper 6	Person6
+2023-01-07	10:30	review by Person7 on paper 7	Person7
+2023-01-08	10:35	Decision REJECT on paper 8	Person8
+2023-01-09	10:40	Decision REJECT on paper 9	Person9
+2023-01-10	10:45	Decision ACCEPT on paper 10	Perso10
 """
 
 def hash(str):
@@ -62,6 +62,8 @@ def processActivity(str):
 rows_list = []
 for line in log.split("\n"):
   row = line.split("\t")
+  if len(row) != 4:
+    continue
   dic = processActivity(row[2])
   dic.update({'time':row[0] + " " + row[1], 'resource':hash(row[3])})
   rows_list.append(dic)
